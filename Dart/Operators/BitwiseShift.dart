673a85ca688ec47d2ca11f64fd8cef0f5124e109
nicmach <morilanger2005@gmail.com>
2022-09-11 16:23:51 +0200

    Bitwise Operations

---
+++ Dart/Operators/BitwiseShift.dart
@@ -0,0 +1,12 @@
+main() {
+  var A = 12;
+  var B = 5;
+
+  print(~A); // Complement of A
+  print(~B); // Complement of B
+  print(A & B); // Bitwise AND
+  print(A | B); // Bitwise OR
+  print(A ^ B); // Bitwise XOR
+  print(A << 2); // Shift Left
+  print(A >> B); // Shift Right
+}
